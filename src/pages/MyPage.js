20eaa305e23fb9c0ddbbb3034912bffbd06c57c0
jiyeon105 <kjy8475@naver.com>
2022-05-31 01:18:35 +0900

    마이페이지 수정

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -6,7 +6,7 @@ import './MyPage.css'
 import axios from "axios";
 
 export default function MyPage() {
-    const { register, watch, handleSubmit, errors} = useForm();
+    const { register, watch, handleSubmit, formState: {errors}} = useForm();
     const navigate = useNavigate();
 
 
@@ -33,28 +33,45 @@ export default function MyPage() {
                     <h1 className="mypage-h1">MyPage</h1>
                     <label className="mypage-label">id</label>
                     <input className="mypage-input"
-                        {...register("email", { required: "email error" } )}
+                        {...register("email", { required: "이메일을 입력해 주세요.",
+                        pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: "유효하지 않은 이메일 형식입니다."
+                                  } } )}
                         type = "email"
                         placeholder = "email"
                         />
+                    {errors.email && errors.email.message && <div className="error">{errors.email.message}</div>}
                     <label className="mypage-label">password</label>
                     <input className="mypage-input"
-                        {...register("pw", { required: "pw error",
+                        {...register("pw", { required: "비밀번호를 입력해 주세요.",
                         minLength:{
                             value:8,
-                            message:"비밀번호는 8자 이상이어야 합니다,"} })}
+                            message:"비밀번호는 8자 이상이어야 합니다."} })}
                         type = "password"
-                        placeholder = "pw"
+                        placeholder = "Password"
                         />
+                    {errors.pw && errors.pw.message && <div className="error">{errors.pw.message}</div>}
+                    {/* <label className="mypage-label">check password</label>
+                    <input
+                        {...register("pwCheck", { required: "비밀번호를 다시 입력해 주세요.",
+                        validate: (value) =>
+                            value === pw.current,
+                            message:"비밀번호가 일치하지 않습니다." })}
+                        type = "password"
+                        placeholder = "Password Check"
+                        /> */}
+                    {/* {errors.pwCheck && errors.pwCheck.value === "validate" && <div className="error">{errors.pwCheck.message}</div>} */}
                     <label className="mypage-label">nickname</label>
                     <input className="mypage-input"
-                        {...register("nickname", { required: "nickname error" ,
+                        {...register("nickname", { required: "닉네임을 입력해 주세요." ,
                         minLength:{
                             value:2,
-                            message:"닉네임은 2글자 이상이어야 합니다,"} })}
+                            message:"닉네임은 2글자 이상이어야 합니다."} })}
                         type = "text"
                         placeholder = "nickname"
                         />
+                    {errors.nickname && errors.nickname.message && <div className="error">{errors.nickname.message}</div>}
                     <label className="mypage-label">회원 유형</label>
                     <div className="memberType">
                     <div className="type-check">
@@ -70,7 +87,7 @@ export default function MyPage() {
                                 일반 회원
                         </label>
                     </div>
-                    <div className="mypage-check">
+                    <div className="mypage-type-check">
                         <label htmlFor="teacher">
                             <input
                                 {...register("type", { required: true })}
